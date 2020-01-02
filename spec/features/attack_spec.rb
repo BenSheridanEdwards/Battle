d17d55428303ce1209bcce13a58df6d78c4663b0
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-02 15:56:01 +0000

    Created attack and confirm method to dry tests

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -15,21 +15,20 @@ feature 'Attack' do
       expect(page).to have_content "It's Leonidas's turn"
     end
 
-    scenario "reduce player 2's hit points by 10" do
+    scenario "reduce Player 2's hit points by 10" do
       click_button 'OK'
       expect(page).not_to have_content 'Leonidas: 60 HP'
       expect(page).to have_content 'Leonidas: 50 HP'
     end
   end
 
-  describe "when it's player 2's turn, and player 2 attacks player 1" do
+  describe "when it's Player 2's turn, and Player 2 attacks Player 1" do
     before(:each) do
       sign_in_and_play
-      click_button 'Attack'
-      click_button 'OK'
+      attack_and_confirm
     end
 
-    scenario "show it's player 2 turn" do
+    scenario "show it's Player 2's turn" do
       expect(page).to have_content "It's Leonidas's turn"
     end
   
@@ -38,7 +37,7 @@ feature 'Attack' do
       expect(page).to have_content "Leonidas attacked Ben!"
     end
 
-    scenario "reduce Player 1 HP by 10 " do
+    scenario "reduce Player 1's HP by 10" do
       click_button 'Attack'
       click_button 'OK'
       expect(page).not_to have_content 'Ben: 60 HP'
@@ -46,8 +45,7 @@ feature 'Attack' do
     end
 
     scenario "be player 1's turn after the attack" do
-      click_button 'Attack'
-      click_button 'OK'
+      attack_and_confirm
       expect(page).to have_content "It's Ben's turn"
     end
   end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -3,4 +3,9 @@ def sign_in_and_play
   fill_in :player_1_name, with: 'Ben'
   fill_in :player_2_name, with: 'Leonidas'
   click_button 'Submit'
-end 
+end
+
+def attack_and_confirm
+  click_button 'Attack'
+  click_button 'OK'
+end
