4ed04c4bf9ee72413b4aba7dea294487a65a620f
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-03 14:28:18 +0000

    Rewording tests for readablity in rspec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,34 +5,35 @@ describe 'Game' do
   let(:player_1) { Player.new("Ben") }
   let(:player_2) { Player.new("Leonidas") }
 
-  describe '#player_1' do
+  describe '.player_1' do
     it 'should return the first player' do 
       expect(game.player_1).to eq player_1
     end
   end
 
-  describe '#player_2' do
+  describe '.player_2' do
     it 'should return the second player' do 
       expect(game.player_2).to eq player_2
     end
   end
 
-  describe '#attack' do 
-    it "should accept a player as an argument and reduce their HP by 10" do
+  describe '.attack' do 
+    it "should reduce a given player's HP by a random number (10)" do
       allow(Kernel).to receive(:rand).and_return(10)
       expect{ game.attack(player_2) }.to change { player_2.hp }.by (-10)
     end
   end
 
-  describe '#switch_turns' do
+  describe '.switch_turns' do
     it 'switches the turn' do
+      expect(game.current_turn).to eq player_1
       game.switch_turns
       expect(game.current_turn).to eq player_2
     end
   end
 
-  describe '#opponent_of' do
-    it 'finds the opponent of a player' do
+  describe '.opponent_of' do
+    it 'finds the opponent of a given player' do
       expect(game.opponent_of(player_1)).to eq player_2
       expect(game.opponent_of(player_2)).to eq player_1
     end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,16 +5,25 @@ describe 'Player' do
   let(:player_1) { Player.new("Ben") }
   let(:player_2) { Player.new("Leonidas") }
 
-  it "should initialise with a given name, and return it to the user when name is called" do
-    expect(player_1.name).to eq("Ben")
+  describe '.name' do
+    it "should return a player's name" do
+      expect(player_1.name).to eq("Ben")
+      expect(player_2.name).to eq("Leonidas")
+    end
   end
 
-  it "should initialize with the default HP value, and return it to the user when hp is called" do 
-    expect(player_1.hp).to eq(Player::DEFAULT_HIT_POINTS)
+  describe '.hp' do
+    it "should return a player's hit points (HP)" do
+      expect(player_1.hp).to eq(60)
+    end
+
+    it 'should return the default HP value, provided no HP value was given to player' do
+      expect(player_1.hp).to eq(Player::DEFAULT_HIT_POINTS)
+    end
   end
 
-  describe '#receive_damage' do
-    it "should reduce the player's HP by 10 when attacked" do
+  describe '.receive_damage' do
+    it "should reduce the player's HP by a random number (10) when attacked" do
       allow(Kernel).to receive(:rand).and_return(10)
       expect{ player_1.attack(player_2) }.to change { player_2.hp }.by (-10)
     end
