5e5fcbe4d4badfee74a972cd394733873844cb1a
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-03 14:57:27 +0000

    Implement game over

--- spec/features/game_over_spec.rb
+++ spec/features/game_over_spec.rb
@@ -1,14 +1,26 @@
 feature 'Game over' do
-  # context 'When Player 1 reaches 0 HP first' do
-  #   before do
-  #     sign_in_and_play
-  #     attack_and_confirm
-  #     allow(Kernel).to receive(:rand).and_return(60)
-  #   end
+  context 'When Player 1 reaches 0 HP first' do
+    before(:each) do
+      sign_in_and_play
+      attack_and_confirm
+      allow(Kernel).to receive(:rand).and_return(60)
+    end
 
-  #   scenario 'Player 1 loses' do
-  #     click_button 'Attack'
-  #     expect(page).to have_content 'Leonidas loses!'
-  #   end
-  # end
+    scenario 'Player 1 loses' do
+      click_button 'Attack'
+      expect(page).to have_content 'Ben loses!'
+    end
+  end
+
+  context 'When Player 2 reaches 0 HP first' do
+    before(:each) do
+      sign_in_and_play
+      allow(Kernel).to receive(:rand).and_return(60)
+    end
+
+    scenario 'Player 2 loses' do
+      click_button 'Attack'
+      expect(page).to have_content 'Leonidas loses!'
+    end
+  end
 end
